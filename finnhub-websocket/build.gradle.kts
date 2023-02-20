939c2c8b70447ea3fe498b27f8b2fd0ae7ce2f39
yokeru <jozef.krupik@hibernix.com>
2023-02-20 18:22:29 +0100

    Rollback gradle and remove signing GPG eval

--- finnhub-websocket/build.gradle.kts
+++ finnhub-websocket/build.gradle.kts
@@ -141,10 +141,6 @@ publishing {
 }
 
 signing {
-    useInMemoryPgpKeys(
-        System.getenv("GPG_PRIVATE_KEY"),
-        System.getenv("GPG_PRIVATE_PASSWORD")
-    )
     sign(publishing.publications)
 }
 
